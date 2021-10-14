8a8dbc9099b703e7fd55eda15e47dfd93b365c8c
Jaifran Hernandez <jaifranjose@gmail.com>
2021-10-13 23:59:32 -0400

    Conecting to my DB

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log("hello");
+import { MikroORM } from "@mikro-orm/core";
+
+const main = async () => {
+    const orm = await MikroORM.init({
+        dbName: 'reddit-db',
+        user: 'postgres',
+        password: 'admin',
+        type: 'postgresql',
+        debug: process.env.NODE_ENV
+    });
+
+    console.log("hello");
+}
+
+main();
+
